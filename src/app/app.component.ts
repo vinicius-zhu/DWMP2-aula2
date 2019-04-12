ff15cf27ab360162d2fc28c844a00c0d0a139fc7
Vinicius Zhu <vinicius.zhu@gmail.com>
2019-04-12 20:40:12 -0300

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'aula2';
+  persons = ['Rodrigo','Henrique','Ramos'];
 }
---
+++ src/app/persons/persons.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from "@angular/core";
+
+@Component({
+  selector:'app-persons',
+  templateUrl:'./persons.component.html',
+  styleUrls:['./persons.component.css']
+})
+
+export class PersonsComponent{
+  @Input() personsList: string[];
+}
