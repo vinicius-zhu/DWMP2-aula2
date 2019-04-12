a0ab4556fc8a2be3eda07dda6f45dee53ecd4ff0
Vinicius Zhu <vinicius.zhu@gmail.com>
2019-04-12 20:48:26 -0300

    Criando campo de input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { PersonsComponent } from './persons/persons.component';
+import { PersonInputComponent } from './persons/person-input.component';
 
 @NgModule({
   declarations: [
-    AppComponent, PersonsComponent
+    AppComponent, PersonsComponent, PersonInputComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/persons/person-input.component.ts
@@ -0,0 +1,9 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector:'app-person-input',
+  templateUrl:'./person-input.component.html',
+  styleUrls:['./person-input.component.css']
+})
+
+export class PersonInputComponent{}
